fa65753cdef05f6c286f4fc5c6fd84ada03bf442
Stef <stef@info.nl>
2015-07-28 11:40:57 +0200

    - Added scheduling possibility in publishing - Fixed temp workaround importing

--- src/main/java/nl/tricode/magnolia/blogs/dialog/action/SaveWordpressBlogDialogAction.java
+++ src/main/java/nl/tricode/magnolia/blogs/dialog/action/SaveWordpressBlogDialogAction.java
@@ -379,10 +379,18 @@ public class SaveWordpressBlogDialogAction extends AbstractAction<SaveWordpressB
 	 * firstName. lastName = eric firstName = tabli The node name is etabli
 	 */
 	private String defineNodeName(final Node node) throws RepositoryException {
+		String result;
 		String intitialFirstName = node.getProperty("firstName").getString();
 		String firstName = StringUtils.isNotBlank(intitialFirstName) ? intitialFirstName.trim() : intitialFirstName;
-		String lastName = node.getProperty("lastName").getString().trim();
-		return Path.getValidatedLabel((firstName.charAt(0) + lastName.replaceAll("\\s+", "")).toLowerCase());
+
+		if (StringUtils.isNotBlank(firstName)) {
+			String lastName = node.getProperty("lastName").getString().trim();
+			result = Path.getValidatedLabel((firstName.charAt(0) + lastName.replaceAll("\\s+", "")).toLowerCase());
+		} else {
+			log.debug("Incomplete wordpress userdetails");
+			result = "Anonymous";
+		}
+		return result;
 	}
 
 	/**
@@ -393,8 +401,14 @@ public class SaveWordpressBlogDialogAction extends AbstractAction<SaveWordpressB
 	private void finishImport() throws ActionExecutionException {
 		try {
 			blogSession.save();
-			contactSession.save();
-			if (shouldImportImages) damSession.save();
+
+			if (shouldImportContacts) {
+				contactSession.save();
+			}
+
+			if (shouldImportImages) {
+				damSession.save();
+			}
 			BlogPostImageImporter.cleanRecentUrlList();
 			callback.onSuccess(getDefinition().getName());
 			shell.openNotification(MessageStyleTypeEnum.INFO, true, "Import completed successfully.");
@@ -412,8 +426,14 @@ public class SaveWordpressBlogDialogAction extends AbstractAction<SaveWordpressB
 	private void cancelImport() throws ActionExecutionException {
 		try {
 			blogSession.refresh(false);
-			contactSession.refresh(false);
-			if (shouldImportImages) damSession.refresh(false);
+
+			if (shouldImportContacts) {
+				contactSession.refresh(false);
+			}
+
+			if (shouldImportImages) {
+				damSession.refresh(false);
+			}
 			BlogPostImageImporter.cleanRecentUrlList();
 			callback.onCancel();
 		} catch (RepositoryException e) {
